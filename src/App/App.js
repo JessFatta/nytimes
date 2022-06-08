6123b4f568fad2490a784f2438084a839e61561c
Jessica Fatta <jessicafatta@gmail.com>
2022-06-08 14:23:57 -0400

    Implement router

--- src/App/App.js
+++ src/App/App.js
@@ -1,24 +1,43 @@
 import React, { Component } from 'react'
 import { getArticles } from '../apiCalls'
+import AllArticles from '../AllArticles/AllArticles'
+import CurrentArticle from '../CurrentArticle/CurrentArticle'
+import { Route } from 'react-router-dom'
+import './App.css'
+
 
 class App extends Component {
   constructor() {
     super()
     this.state = {
       articles: [],
-      currentArticle: ''
+      currentArticle: '',
+      error: null
     }
   }
 
   componentDidMount() {
     getArticles()
-    .then(data => this.setState({articles: data}))
+    .then(data => this.setState({articles: data.response.docs}))
+    .catch(error => this.setState({error: error}))
   }
 
   render() {
     return (
-      <div>
-        <h1>hey</h1>
+      <div className='main'>
+        <h1>NY Times</h1>
+      <Route exact path='/' render={() => {
+        return (
+
+          <AllArticles articles={this.state.articles} />
+        )
+      }
+        } />
+        <Route exact path='/:id' render={(match) => {
+          return (
+          <CurrentArticle details={this.state.articles[match.match.params.id]} />
+          )
+        }} />
       </div>
     )
   }
