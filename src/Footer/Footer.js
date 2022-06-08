25b8e63d1cfc51e781a4f4d178c6aece7078ee75
Jessica Fatta <jessicafatta@gmail.com>
2022-06-08 14:31:35 -0400

    Add footer with styling

---
+++ src/Footer/Footer.js
@@ -0,0 +1,18 @@
+
+   
+import React from "react"
+import "./Footer.css"
+
+
+const Footer = () => {
+  return(
+    <footer>
+      <p className="footer-text">©2022 Da Times by Jess Fatta</p>
+      <p className="footer-text">|</p>
+      <p className="footer-text">Real News, Fake Site</p>
+    </footer>
+  )
+}
+
+
+export default Footer
