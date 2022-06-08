e21fae7545ae2f1ef167fcd891e5d4ac2ba2e887
Jessica Fatta <jessicafatta@gmail.com>
2022-06-08 15:13:11 -0400

    Comment out image for now

--- src/CurrentArticle/CurrentArticle.js
+++ src/CurrentArticle/CurrentArticle.js
@@ -4,10 +4,12 @@ import './CurrentArticle.css'
 const CurrentArticle = ({details}) => {
   return (
     <div className='details-view'>
-      <h4>{details.abstract}</h4>
-      <p>{details.lead_paragraph}</p>
+      <h3>{details.headline.main}</h3>
       <p>{details.byline.original}</p>
-      <p>read more:</p><a href={details.web_url}>{details.web_url}</a>
+      <p>{details.abstract}</p>
+      <p>{details.lead_paragraph}</p>
+      <a href={details.web_url}>Click to read full article</a>
+      {/* <img src={details.multimedia[0].url} /> */}
     </div>
   )
 }
