3d6afadff9af2b283f57a07071b5ac7db4ccdea9
Jessica Fatta <jessicafatta@gmail.com>
2022-06-10 13:04:31 -0400

    Add link to filtered articles

--- src/FilteredArticles/FilteredArticles.js
+++ src/FilteredArticles/FilteredArticles.js
@@ -1,15 +1,20 @@
 import React from 'react'
 import ArticleCard from '../ArticleCard/ArticleCard'
+import { Link } from "react-router-dom";
+
 
 const FilteredArticles = ({filteredArticles, section}) => {
-  const filteredArticlesCards = filteredArticles.map(article => {
+  const filteredArticlesCards = filteredArticles.map((article, i) => {
     return (
+      <Link to={`/${i}`}>
       <ArticleCard 
         abstract={article.headline.main}
         section={article.subsection_name}
         url={article.web_url}
-        key={article._id}
+        key={i}
+        id={article._id}
         />
+        </Link>
     )
   })
 
