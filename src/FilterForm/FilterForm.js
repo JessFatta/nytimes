7ee3a42757f29138657ed9517a658fd83a4540b8
Jessica Fatta <jessicafatta@gmail.com>
2022-06-10 10:41:30 -0400

    CLean up code

--- src/FilterForm/FilterForm.js
+++ src/FilterForm/FilterForm.js
@@ -2,20 +2,18 @@ import React from 'react'
 import './FilterForm.css'
 
 const FilterForm = ({getSections}) => {
-   const handleChange = (section) => {
+  const handleChange = (section) => {
     getSections(section)
   }
 
   return (
     <div className='section-filter'>
-      <label className='filter-label' name='designation'>Browse By Article Section</label>
+      <label className='filter-label' name='designation'>Browse By Article Section </label>
       <select className='section' name='section' onChange={({target: {value}}) => handleChange(value)}>
         <option value='All'>All Sections</option>
         <option value='Australia'>Australia</option>
         <option value='Europe'>Europe</option>
         <option value='Politics'>Politics</option>
-        <option value=''></option>
-        <option value=''></option>
       </select>
     </div>
   )
